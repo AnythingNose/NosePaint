ccdcae4af9441f70f8eecadb10c9e48976854bf9
AnythingNose <anythingnoseproductions@gmail.com>
2024-12-05 17:42:42 +0000

    Populated menu more and added file dialogue
    - File dialogue doesn't do anything yet.

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,5 +1,7 @@
 class_name Main extends Control
 
+@onready var file_dialog: FileDialog = $FileDialog
+
 var block_draw : bool = false:
 	set(value):
 		block_draw = value
@@ -11,3 +13,27 @@ func _ready() -> void:
 func show_mouse(should_show : bool) -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE if should_show or (%Canvas.radius == 0) else Input.MOUSE_MODE_HIDDEN
 	%Crosshair.visible = !should_show and (%Canvas.radius != 0)
+
+# File save / load functions
+func file_open_dialog() -> void:
+	file_dialog.file_mode = FileDialog.FILE_MODE_OPEN_FILE
+	file_dialog.clear_filters()
+	file_dialog.add_filter("*.png, *.jpg", "Images")
+	file_dialog.add_filter("*", "All Files")
+	file_dialog.popup_centered()
+
+func file_save_dialog() -> void:
+	file_dialog.file_mode = FileDialog.FILE_MODE_SAVE_FILE
+	file_dialog.clear_filters()
+	file_dialog.add_filter("*.png", "PNG")
+	file_dialog.add_filter("*.jpg", "JPEG")
+	file_dialog.popup_centered()
+
+# File dialogue callbacks
+func _on_file_dialog_file_selected(path: String) -> void:
+	match file_dialog.file_mode:
+		FileDialog.FILE_MODE_OPEN_FILE:
+			print("open" + path)
+		FileDialog.FILE_MODE_SAVE_FILE:
+			print("save"+ path)
+	
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -4,11 +4,15 @@ extends Control
 @onready var menu_label: Label = $MenuLabel
 
 # Setting buttons 
-@onready var fullscreen_button: Button = $Menu/MenuHBox/Settings/SettingsVBox/Fullscreen/Fullscreen
-
-var open : bool = false
+@onready var fullscreen_button: Button = $Menu/MenuHBox/ScrollContainer/VBoxContainer/SettingsVBox/Fullscreen/Fullscreen
 
 var main : Main
+var open : bool = false
+
+var hide_menu_label : bool:
+	set(value):
+		hide_menu_label = value
+		show_menu_label(value)
 
 func _ready() -> void:
 	main = get_tree().current_scene
@@ -25,8 +29,11 @@ func _input(event: InputEvent) -> void:
 func show_menu(should_show : bool) -> void:
 	menu.visible = should_show
 	main.block_draw = should_show
-	menu_label.visible = !should_show
+	show_menu_label(!should_show)
 	%Palette.visible = !should_show
+
+func show_menu_label(should_show : bool) -> void:
+	menu_label.visible = false if hide_menu_label else should_show
 
 # Settings
 func _on_fullscreen_toggled(toggled_on: bool) -> void:
@@ -35,3 +42,22 @@ func _on_fullscreen_toggled(toggled_on: bool) -> void:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+func _on_hide_menu_label_toggled(toggled_on: bool) -> void:
+	hide_menu_label = toggled_on
+
+
+func _on_new_image_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_open_image_pressed() -> void:
+	main.file_open_dialog()
+
+
+func _on_save_image_pressed() -> void:
+	main.file_save_dialog()
+
+
+func _on_save_image_as_pressed() -> void:
+	pass # Replace with function body.
