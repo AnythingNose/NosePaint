1b76877a3fbc6ed5d6af8824e8f2e1427ae9042c
AnythingNose <anythingnoseproductions@gmail.com>
2024-12-05 00:21:37 +0000

    Set default colours to UFO 50 palette
    https://lospec.com/palette-list/ufo-50

--- scripts/palette.gd
+++ scripts/palette.gd
@@ -1,22 +1,51 @@
 class_name ColourPalette extends HBoxContainer
 
-@onready var default_white: ColourButton = $ColorRect
+@onready var archetype : ColourButton = $ColorRect
 
 # White is included by the default colour button.
-const colours : Array[Color] = [
-	Color.BLACK,
-	Color.RED,
-	Color.ORANGE,
-	Color.YELLOW,
-	Color.GREEN,
-	Color.BLUE,
+const colours : Array[String] = [
+	"ffffff",
+	"a48080",
+	"feb854",
+	"e8ea4a",
+	"58f5b1",
+	"64a4a4",
+	"cc68e4",
+	"fe626e",
+	"c8c8c8",
+	"e03c32",
+	"fe7000",
+	"63b31d",
+	"a4f022",
+	"27badb",
+	"fe48de",
+	"d10f4c",
+	"707070",
+	"991515",
+	"ca4a00",
+	"a3a324",
+	"008456",
+	"006ab4",
+	"9600dc",
+	"861650",
+	"000000",
+	"4c0000",
+	"783450",
+	"8a6042",
+	"003d10",
+	"202040",
+	"340058",
+	"4430ba",
 ]
 
+# TODO: Fade the pallette UI out when the user is drawing
+
 func _ready() -> void:
-	for colour in colours:
-		var s : ColorRect = default_white.duplicate()
+	for colour : String in colours:
+		var s : ColorRect = archetype.duplicate()
 		add_child(s)
-		s.color = colour
+		s.color = Color(colour)
+	archetype.hide()
 
 func select_colour(button : ColourButton) -> void:
 	%Canvas.colour_primary = button.color
